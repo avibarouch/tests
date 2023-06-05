40fa45e4036ba04c7aabea746372ec7da68118ff
Avi Barouch <avibarouch@gmail.com>
2023-06-05 21:39:33 +0300

    Add "hello World" response for chackin on postman

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const userController = require('../controller/user');
 
 const router = express.Router();
 router.post('/user', userController.createUser);
-
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 module.exports = router;
